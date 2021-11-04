8c7fdc4fe17f3dafd85e03838ebe59094224c75a
superAnt <87319344+RohSeongHoon@users.noreply.github.com>
2021-11-04 17:28:52 +0900

    카테고리 데이터 수정
    categories 배열로 통합 함

--- categories.js
+++ categories.js
@@ -3,64 +3,73 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 
-var actorCategory = [
-  { category: "actor" },
-  { categoryKr: "배우" },
-  { link: "../test/community/actor.html" },
+var categories = [
   {
-    contents: [
-      { videoPosts: "./community/actor-post-main.html" },
-      { profileBox: "#" },
-      { auditionPosts: "#" },
-      { freePosts: "#" },
+    actorCategories: [
+      { category: "actor" },
+      { categoryKr: "배우" },
+      { link: "../test/community/actor.html" },
+      {
+        contents: [
+          { videoPosts: "./community/actor-post-main.html" },
+          { profileBox: "#" },
+          { auditionPosts: "#" },
+          { freePosts: "#" },
+        ],
+      },
     ],
   },
-];
-var musicalCategory = [
-  { category: "musical" },
-  { categoryKr: "뮤지컬" },
-  { link: "../test/community/musical.html" },
   {
-    contents: [
-      { videoPosts: "#" },
-      { profileBox: "#" },
-      { auditionPosts: "#" },
-      { freePosts: "#" },
+    musicalCategory: [
+      { category: "musical" },
+      { categoryKr: "뮤지컬" },
+      { link: "../test/community/musical.html" },
+      {
+        contents: [
+          { videoPosts: "#" },
+          { profileBox: "#" },
+          { auditionPosts: "#" },
+          { freePosts: "#" },
+        ],
+      },
     ],
   },
-];
-var vocalCategory = [
-  { category: "vocal" },
-  { categoryKr: "보컬" },
-  { link: "../test/community/vocal.html" },
   {
-    contents: [{ videoPosts: "#" }, { auditionPosts: "#" }, { freePosts: "#" }],
+    vocalCategory: [
+      { category: "vocal" },
+      { categoryKr: "보컬" },
+      { link: "../test/community/vocal.html" },
+      {
+        contents: [
+          { videoPosts: "#" },
+          { auditionPosts: "#" },
+          { freePosts: "#" },
+        ],
+      },
+    ],
   },
-];
-var performerCategory = [
-  { category: "performer" },
-  { categoryKr: "연주자 " },
-  { link: "../test/community/performer.html" },
   {
-    contents: [{ videoPosts: "#" }, { auditionPosts: "#" }, { freePosts: "#" }],
+    performerCategory: [
+      { category: "performer" },
+      { categoryKr: "연주자 " },
+      { link: "../test/community/performer.html" },
+      {
+        contents: [
+          { videoPosts: "#" },
+          { auditionPosts: "#" },
+          { freePosts: "#" },
+        ],
+      },
+    ],
   },
 ];
 
 app.use(cors());
 
-app.get("/actorCategory", (req, res) => {
-  res.json(actorCategory);
-});
-app.get("/musicalCategory", (req, res) => {
-  res.json(musicalCategory);
-});
-app.get("/vocalCategory", (req, res) => {
-  res.json(vocalCategory);
+app.get("/categories", (req, res) => {
+  res.json(categories);
 });
 
-app.get("/performerCategory", (req, res) => {
-  res.json(performerCategory);
-});
 app.listen(port, () => {
   console.log(`example app listening at http://localhost:${port}`);
 });
